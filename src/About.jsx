0931e8d7db1f5a531a9f948b41c4b717a88de20a
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-08-15 21:57:41 +0530

    add about component

---
+++ src/About.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-unescaped-entities */
+const About = () => {
+  return (
+    <div className="m-2 mt-10 p-2">
+      <h1 className="text-3xl font-bold text-center m-2 p-2"> About Me </h1>
+      <p className="p-2 m-2 pb-0 mb-0">
+        Hey there, I'm Pulkit Agrawal—a dedicated full stack developer who
+        thrives on crafting scalable web applications that make a difference.
+        Transforming ideas into impactful code and solving real-world challenges
+        energize me. Armed with a computer science engineering degree, I possess
+        a solid foundation in technology. My professional journey thrives in
+        fast-paced environments, where I've honed a keen interest in finance and
+        startups. Proficient in technologies like Javascript, ReactJs, and the
+        MERN stack, I'm driven to innovate and deliver top-notch solutions.
+      </p>
+      <p className="px-2 mx-2">
+        When I'm not immersed in code, you can find me on the cricket field,
+        indulging in Netflix binges, or exploring new literary worlds through
+        books. Beyond my tech pursuits, I thrive on connecting with fresh faces
+        over a steaming cup of coffee—a great opportunity for stimulating
+        conversations and potential collaborations. Feel free to connect with me
+        on LinkedIn, Twitter, or drop me an email. Let's explore how we can
+        contribute to the world of technology and beyond.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./About";
 import Header from "./Header";
 import Introduction from "./Introduction";
 
@@ -6,6 +7,7 @@ const App = () => {
     <>
       <Header />
       <Introduction />
+      <About />
     </>
   );
 };
