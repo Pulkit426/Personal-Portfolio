111d94a5fa3c562d9a401b8f1eb7396e11338e4f
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-08-15 19:02:03 +0530

    add header component

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <div className="h-20 flex justify-between px-2 py-6 bg-gray-400">
+      <h1> Pulkit Agrawal </h1>
+
+      <nav className="pr-10">
+        <ul className="flex flex-row gap-6">
+          <li> Home </li>
+          <li> About  </li>
+          <li> Skills </li>
+          <li> Project </li>
+          <li> Experience </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
