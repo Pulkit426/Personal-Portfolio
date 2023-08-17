92802ce79e8599c5e2bd7a2589a777709c03dc3a
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-08-17 23:39:26 +0530

    add logos for skills section

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,7 +1,44 @@
+
 const Skills = () => {
   return (
     <div>
-      Skills
+     <h1 className="text-3xl font-bold text-center p-2 m-2"> Skills </h1> 
+
+     <div className="flex">
+
+
+      <div> 
+        Frontend
+
+     <img src="src\assets\htmllogo.png" className="w-24 h-24 p-2"  /> 
+     <img src="src\assets\csslogo.png" className="w-24 h-24 p-2"  /> 
+
+     <img src="src\assets\reduxlogo.png" className="w-24 h-24 p-2"  /> 
+     <img src="src\assets\tailwindcsslogo.png" className="w-24 h-24 p-2"  />
+     </div>
+
+     <div>
+      Back end
+
+      <img src="src\assets\nodelogo.png" className="w-24 h-24 p-2"  /> 
+      <img src="src\assets\expresslogo.png" className="w-52 h-24 p-2"  /> 
+     </div>
+
+     <div>
+      Languages
+      <img src="src\assets\c++.png" className="w-24 h-24 p-2"  /> 
+      <img src="src\assets\jslogo.png" className="w-24 h-24 p-2"  /> 
+     </div>
+
+     <div>
+      Testing
+           <img src="src\assets\jest.png" className="w-24 h-24 p-2"  /> 
+     <img src="src\assets\react-testing-library.png" className="w-24 h-24 p-2"  /> 
+     <img src="src\assets\cypress.png" className="w-24 h-24 p-2 bg-slate-300"  /> 
+     </div>
+
+
+     </div>
     </div>
   )
 }
